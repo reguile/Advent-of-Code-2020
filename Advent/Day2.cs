a80178a18a32ffd5081507b0f556bc5756f4943d
Ryan Guile <ryanguile@hotmail.com>
2020-12-02 13:21:06 -0500

    Added day 2 part 2 solution

--- Advent/Day2.cs
+++ Advent/Day2.cs
@@ -49,5 +49,43 @@ class Day2 {
         Console.WriteLine("There are {0} valid passwords", valid);
         Console.ReadLine();
     }
+
+    public static void day2part2() {
+        // I'm not sure if this is the best way to do this. I stuck the text file in the product directory.
+        string path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), @"passwords_day2.txt");
+
+        int pos1 = 0;
+        int pos2 = 0;
+        int valid = 0;
+
+        char letter;
+
+        string password;
+        string[] substring;
+
+        char[] seperators = new char[] { '-', ' ', ':' };
+
+        // Read the list of numbers, standard stuff
+        using (StreamReader sr = new StreamReader(path)) {
+            string line;
+            while ((line = sr.ReadLine()) != null) {
+                substring = line.Split(seperators);
+                pos1 = Convert.ToInt16(substring[0]) - 1;
+                pos2 = Convert.ToInt16(substring[1]) - 1;
+                letter = substring[2][0];
+                password = substring[4];
+
+                // there's got to be a cleaner way to do this.
+                if((password[pos1] == letter && password[pos2] != letter) || (password[pos1] != letter && password[pos2] == letter)) {
+                    valid++;
+                }
+
+
+            }
+        }
+
+        Console.WriteLine("There are {0} valid passwords", valid);
+        Console.ReadLine();
+    }
 }
 
--- Advent/Program.cs
+++ Advent/Program.cs
@@ -4,7 +4,7 @@ namespace Advent {
     class Program {
 
         static void Main(string[] args) {
-            Day2.day2part1();
+            Day2.day2part2();
                      
         }
     }
